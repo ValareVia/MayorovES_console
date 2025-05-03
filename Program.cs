833aa7382522a92479236fa812c6810adc0d8e66
ValareVia <Timetolegend@gmail.com>
2025-05-03 08:07:49 +0300

    Add *.cs-files

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// МЕС Ам-1-24
+
+
+
+
+double Х = Convert.ToInt32(Console.ReadLine());
+double r = Sqr(Х);
+
+static double Sqr(double X)
+{
+    return X * X;
+}
+
+
+
+
+Console.WriteLine("Квадрат числа " + Х + ":" + r);
